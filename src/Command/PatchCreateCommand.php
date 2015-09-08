61ec92f8a094b6d10d07772c46b7f5440d8ed210
Alexandre ALTUN <alexandre.altun@gmail.com>
2015-09-08 16:22:53 +0200

    fix bug in patch:create

--- src/Command/PatchCreateCommand.php
+++ src/Command/PatchCreateCommand.php
@@ -25,24 +25,8 @@ class PatchCreateCommand extends DbvcCommand
         try {
             $patch = $this->dbvc->createNewPatch($name);
             $output->writeln("Patch created");
-
         } catch (Exception $e) {
             $output->writeln("This patch already exists");
-        }
-
-
-
-        $filePath = rtrim(getcwd(), '/') . '/dbvc.xml';
-
-        if (file_exists($filePath)) {
-            $output->writeln("The config file <info>$filePath</info> already exists");
-        } else {
-            if (@copy(__DIR__ . '/../../config.example.xml', $filePath)) {
-                $output->writeln("Config file <info>$filePath</info> created");
-                $output->writeln("Please edit it to configure dbvc");
-            } else {
-                $output->writeln("<error>Unable to create file $filePath. Is the directory writeable ?</error>");
-            }
         }
     }
 }
