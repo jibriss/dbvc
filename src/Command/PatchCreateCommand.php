8aac018478ceb2512c84c1eb0d99d7f294ee9f2a
Alexandre ALTUN <alexandre.altun@gmail.com>
2015-11-20 10:19:19 +0100

    patch:create : add option --with-date

--- src/Command/PatchCreateCommand.php
+++ src/Command/PatchCreateCommand.php
@@ -4,6 +4,7 @@ namespace Jibriss\Dbvc\Command;
 use Exception;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class PatchCreateCommand extends DbvcCommand
@@ -15,6 +16,7 @@ class PatchCreateCommand extends DbvcCommand
         $this
             ->setName('patch:create')
             ->addArgument('patch_name', InputArgument::REQUIRED, 'Name of the patch to create')
+            ->addOption('with-date', 'd', InputOption::VALUE_NONE, 'Prefix the patch name with the current date')
             ->setDescription('Create new empty patch files : migration and rollback')
         ;
     }
@@ -22,6 +24,10 @@ class PatchCreateCommand extends DbvcCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $name = $input->getArgument('patch_name');
+
+        if ($input->hasOption('with-date')) {
+            $name = date('YmdHis').'-'.$name;
+        }
 
         try {
             $patch = $this->dbvc->createNewPatch($name);
